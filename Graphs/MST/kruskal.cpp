83d8656ffb530328ef53f80ef5ec664ddfac74f7
MaetamongVH <lvt11072000@gmail.com>
2020-06-28 23:59:39 +0700

    update kruskall

--- Graphs/MST/kruskal.cpp
+++ Graphs/MST/kruskal.cpp
@@ -13,12 +13,18 @@ int cost[n][n] = {
     {6, 0, 0, 8, 0, 2},
     {5, 4, 4, 5, 2, 0}};
 
-vector<int> Tree;
+vector<pair<int, int>> Tree;
 int d = 0;
 vector<pair<int, pair<int, int>>> edges;
+int root[n];
 
 void init()
 {
+    Tree.clear();
+    for (int i = 0; i < n; i++)
+    {
+        root[i] = i;
+    }
     for (int i = 0; i < n; i++)
     {
         for (int j = i + 1; j < n; j++)
@@ -41,14 +47,58 @@ void print()
         int weight = edges[i].first;
         int u = edges[i].second.first;
         int v = edges[i].second.second;
-        cout << "edge(" << u << "," << v << ") : " << weight << endl;
+        cout << "edge(" << u + 1 << "," << v + 1 << ") : " << weight << endl;
+    }
+}
+void kruskal()
+{
+    for (int i = 0; i < edges.size(); i++)
+    {
+        int u = edges[i].second.first;
+        int v = edges[i].second.second;
+        if (root[u] != root[v])
+        {
+            cout << "truoc duyet: " << endl;
+            cout << "root cua " << u + 1 << " la " << root[u] + 1 << endl;
+            cout << "root cua " << v + 1 << " la " << root[v] + 1 << endl;
+
+            pair<int, int> p = make_pair(u, v);
+            Tree.push_back(p);
+            cout << "trong khi duyet: " << endl;
+            for (int j = 0; j < n; j++)
+            {
+                int currRoot = root[v];
+                if (root[j] == currRoot)
+                {
+                    cout << "root cua " << j + 1 << " chuyen tu " << root[j] + 1;
+                    root[j] = root[u];
+                    cout << " thanh " << root[j] + 1 << endl;
+                }
+            }
+            cout << "sau duyet: " << endl;
+            cout << "root cua " << u + 1 << " la " << root[u] + 1 << endl;
+            cout << "root cua " << v + 1 << " la " << root[v] + 1 << endl;
+        }
+    }
+    cout << "sau khi duyet xong root cua cac canh la: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "root cua " << i + 1 << " la " << root[i] + 1 << endl;
+    }
+    cout << "so canh trong cay khung: " << Tree.size();
+    cout << "cay khung gom nhung canh: " << endl;
+    for (int i = 0; i < Tree.size(); i++)
+    {
+        int u = Tree[i].first;
+        int v = Tree[i].second;
+        cout << "edge(" << u + 1 << "," << v + 1 << ")" << endl;
     }
 }
 int main()
 {
     init();
     print();
-
+    kruskal();
     return 0;
 }
 
